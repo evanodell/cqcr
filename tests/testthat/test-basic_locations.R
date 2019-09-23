73bff95c92e209fde7fdcfdfe686ab21b881b870
evan <evanodell91@gmail.com>
2019-09-23 15:20:38 +0100

    vignette cleanup [skip ci]

--- tests/testthat/test-basic_locations.R
+++ tests/testthat/test-basic_locations.R
@@ -4,7 +4,6 @@ test_that("basic locations works", {
   location1 <- cqc_location("1-1428643301")
   expect_true(is.list(location1))
 
-
   expect_error(cqc_location("blaah"))
 
   insp_loc1 <- cqc_location_inspection_area("1-122710325")
