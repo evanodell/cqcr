c008a61a24df7e86389206ec252f08f146688003
evan <evanodell91@gmail.com>
2019-09-23 13:53:19 +0100

    add another test [ci skip]

--- tests/testthat/test-basic_locations.R
+++ tests/testthat/test-basic_locations.R
@@ -4,6 +4,9 @@ test_that("basic locations works", {
   location1 <- cqc_location("1-1428643301")
   expect_true(is.list(location1))
 
+
+  expect_error(cqc_location("blaah"))
+
   insp_loc1 <- cqc_location_inspection_area("1-122710325")
 
   expect_true(is.list(insp_loc1))
