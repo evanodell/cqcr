b20611ea0a676a6853de797da8dabfc62770a2e1
evan <evanodell91@gmail.com>
2019-09-23 13:30:42 +0100

    [skip-ci] add comments

--- tests/testthat/test-reports-pdf.R
+++ tests/testthat/test-reports-pdf.R
@@ -12,7 +12,7 @@ test_that("report PDF retrieval works", {
   expect_true(file.exists("5c4e83b5-68da-4bae-941b-694a4cd9e808.pdf"))
 
   file.remove("5c4e83b5-68da-4bae-941b-694a4cd9e808.pdf")
-
+  ## Only run this test locally
 
 
 })
